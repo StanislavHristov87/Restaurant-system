3bd9b2589239d699f988bcad8f1e8e7fb5a6edc3
Stanislav Hristov <stanislavhristov0331@gmail.com>
2025-11-12 23:43:23 +0200

    Initial commit

---
+++ backend/src/controllers/userController.js
@@ -0,0 +1,42 @@
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+// Registration
+
+export const registerUser = async (req, res) => {
+    try {
+        const {name, email, password, role } = req.body;
+
+        const userExists = await User.findOne({ email });
+        if (userExists) return res.status(400).json({ message: "Email already exist" });
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = await User.create({ name, email, password: hashedPassword, role });
+
+        res.status(201).json({ message: "User created successfully", user });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+// log in
+
+export const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email });
+        if (!user) return res.status(400).json({ message: "Invalid credentials" });
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) return res.status(400).json({ message: "Invalid credentials" });
+
+        const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, {
+            expiresIn: "1d",
+        });
+        res.json({ message: "Login successful", token, user });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
